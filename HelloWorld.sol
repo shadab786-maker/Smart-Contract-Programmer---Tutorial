0f21dfde39a7fd4c6d8a6d1baedab07e19aa7fab
shadab786-maker <68143153+shadab786-maker@users.noreply.github.com>
2024-06-16 16:58:02 +0530

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,1 +1,14 @@
 
+// --------------IF we do not specify the license, then the compiler will give ut the warnings.
+
+// SPDX-License-Identifier: MIT
+
+//Best practice is to set the compiler version to a specific version
+pragma solidity 0.8.7;
+
+contract HelloWorld {
+    string public myStirng = "Hello World";
+}
+
+
+
